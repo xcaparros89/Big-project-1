2a58e7ad04963a23c8cea0b22ed4a51843d9e0c6
Xavier Caparros <xcaparros@hotmail.com>
2020-10-08 22:48:48 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+let searchString = '';
+let makeSearchString = (kind, string)=>{
+    searchString += `+${kind}${string}`;
+}
+
+let findBooks = (searchString)=>{
+    fetch(`https://www.googleapis.com/books/v1/volumes?q= ${searchString}`).then(response => response.json()).then(data=>console.log(data))
+}
+
+array = [['intitle:', 'final empire'],['inauthor:', 'Sanderson']]
+
+array.forEach(arr=>{
+    makeSearchString(arr[0], arr[1])
+})
+
+findBooks(searchString)
+
+//intitle: Returns results where the text following this keyword is found in the title.
+//inauthor: Returns results where the text following this keyword is found in the author.
+//inpublisher: Returns results where the text following this keyword is found in the publisher.
+//subject:
